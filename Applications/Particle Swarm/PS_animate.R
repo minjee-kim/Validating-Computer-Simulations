46f89e279b0320cba818643943fc0b23eddb6a3f
Minjee Kim <minjeekim@MacBook-Pro-9.local>
2026-09-15 10:03:27 -0500

    deleting unnecessarily saved plots

--- Applications/Particle Swarm/PS_animate.R
+++ Applications/Particle Swarm/PS_animate.R
@@ -2,60 +2,51 @@
 
 source("particle_movement_ABM.R")
 
-### run the system 
+### run simulation
 abm_run <- vis_ABM(
   total_population = 100,
-  steps = 100,
+  steps = 500,
   movement_speed = 1.3,
+  animate = FALSE,
   seed = 123
 )
-## history tracking
-abm_run %>%
-  filter(step == 50) %>%
-  ggplot(aes(x = x, y = y)) +
-  geom_point(size = 2) +
-  coord_fixed(
-    xlim = c(0, 100),
-    ylim = c(0, 100)
-  ) +
-  labs(
-    title = "Particle Swarm",
-    subtitle = "Step 50",
-    x = "x",
-    y = "y"
-  ) +
-  theme_minimal()
 
 
-## animate 
+### animate afterward
+library(ggplot2)
+library(gganimate)
+library(gifski)
+
 p <- ggplot(
   abm_run,
   aes(x = x, y = y)
 ) +
   geom_point(
-    size = 2,
-    alpha = 0.8
+    size = 1,
+    color = "pink"
   ) +
   coord_fixed(
     xlim = c(0, 100),
-    ylim = c(0, 100),
-    expand = FALSE
+    ylim = c(0, 100)
   ) +
   labs(
-    title = "Particle Swarm Simulation",
-    subtitle = "Step: {frame_time}",
-    x = "x",
-    y = "y"
+    title = "Step: {frame_time}",
+    x = "X Position",
+    y = "Y Position"
   ) +
   theme_minimal() +
-  transition_time(step)
+  transition_time(step) +
+  ease_aes("linear")
+
 
 animate(
   p,
-  nframes = 101,
-  fps = 15,
-  width = 600,
-  height = 600
+  nframes = 100,
+  fps = 10,
+  renderer = gifski_renderer()
 )
+
+
+
 
 
